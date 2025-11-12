69860f4023b430695eab101f872efb0098de2569
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-11-12 22:46:06 +0900

    feat: ツール実行時にスクレイピングが実行されるように main メソッドを修正

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,8 +2,26 @@
 
 import { consola } from "consola";
 
+import { UseCase } from "./usecase/index.ts";
+import { 
+    FetchRepositoryImpl, 
+    PaginationScrapingRepositoryImpl, 
+    RentalPropertyScrapingRepositoryImpl
+} from "./repository/index.ts";
+
 const main = async () => {
-    consola.info("Hello, Moving Tool CLI!");
+    try {
+        const useCase = new UseCase(
+            new FetchRepositoryImpl(),
+            new PaginationScrapingRepositoryImpl(),
+            new RentalPropertyScrapingRepositoryImpl()
+        );
+        await useCase.execute();
+
+        consola.success("[SUCCESS] All tasks completed successfully.");
+    } catch (error) {
+        consola.error("[ERROR] ", error);
+    }  
 };
 
 main();
