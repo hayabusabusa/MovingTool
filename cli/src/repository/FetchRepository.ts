bf7b586d1bf0f3f0b34b887e7c3ea35480db0103
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-11-12 19:17:16 +0900

    fix: GET リクエストを送信してレスポンスを受け取る Repository のインターフェースを修正
    スクレイピングを行う際は文字列から dom を作成するため、レスポンスを文字列として返すように修正。

--- cli/src/repository/FetchRepository.ts
+++ cli/src/repository/FetchRepository.ts
@@ -5,20 +5,20 @@ export interface FetchRepository {
     /**
      * 指定した URL に GET リクエストを送信してレスポンスを受け取る.
      * @param url リクエストを送信する URL
-     * @returns レスポンスデータ
+     * @returns レスポンスデータを文字列にしたもの
      */
-    fetch(url: string): Promise<ArrayBuffer>;
+    fetch(url: string): Promise<string>;
 }
 
 /**
  * `fetch` を利用して指定した URL に GET リクエストを送信してレスポンスを受け取る Repository の実装クラス.
  */
 export class FetchRepositoryImpl implements FetchRepository {
-    async fetch(url: string): Promise<ArrayBuffer> {
+    async fetch(url: string): Promise<string> {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
         }
-        return response.arrayBuffer();
+        return response.text();
     }
 }
