61d93df5e176dc3af537ca7596165ab71854e502
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-11-13 02:24:58 +0900

    feat: `consola` によるログ出力を Repository で実装するように修正
    特定のパッケージに依存しないように Repository 経由でログを表示するように修正。

--- cli/src/repository/index.ts
+++ cli/src/repository/index.ts
@@ -3,14 +3,18 @@ import { FetchRepositoryImpl } from "./FetchRepository.ts";
 import type { ScrapingRepository } from "./ScrapingRepository.ts";
 import { PaginationScrapingRepositoryImpl } from "./PaginationScrapingRepositoryImpl.ts";
 import { RentalPropertyScrapingRepositoryImpl } from "./RentalPropertyScrapingRepositoryImpl.ts";
+import type { Logger } from "./Logger.ts";
+import { LoggerImpl } from "./Logger.ts";
 
 export type { 
     FetchRepository,
+    Logger,
     ScrapingRepository
 };
 
 export {
     FetchRepositoryImpl,
+    LoggerImpl,
     PaginationScrapingRepositoryImpl,
     RentalPropertyScrapingRepositoryImpl
 };
