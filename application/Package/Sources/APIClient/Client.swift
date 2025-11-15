fa8f0aefe1d78513bcdd4eda555d4ad906ca229f
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-11-15 15:36:59 +0900

    feat: API 通信を行うクライアントの実装を追加

--- application/Package/Sources/APIClient/Client.swift
+++ application/Package/Sources/APIClient/Client.swift
@@ -5,3 +5,40 @@
 //  Created by Shunya Yamada on 2025/11/15.
 //
 
+import Dependencies
+import DependenciesMacros
+import Foundation
+import SharedModels
+
+@DependencyClient
+public struct APIClient: Sendable {
+    /// ページ情報を取得する.
+    public var fetchPageInformation: @Sendable () async throws -> PageInformation
+    /// 指定されたページの部屋情報を取得する.
+    public var fetchRooms: @Sendable (_ page: Int) async throws -> [Room]
+}
+
+extension APIClient: TestDependencyKey {
+    public static var previewValue: APIClient {
+        .init {
+            .init(
+                totalPages: 1,
+                totalProperties: 1,
+                updatedAt: Int(Date().timeIntervalSince1970)
+            )
+        } fetchRooms: { page in
+            []
+        }
+    }
+
+    public static var testValue: APIClient {
+        .init()
+    }
+}
+
+extension DependencyValues {
+    public var apiClient: APIClient {
+        get { self[APIClient.self] }
+        set { self[APIClient.self] = newValue }
+    }
+}
