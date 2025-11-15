3b110eb47b48dadada2210a0156b9c39f90abd0c
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-11-15 22:43:53 +0900

    chore: Xcode プロジェクトにアプリ用モジュールへの依存を追加

--- application/Package/Sources/MovingMapFeature/MovingMapView.swift
+++ application/Package/Sources/MovingMapFeature/MovingMapView.swift
@@ -5,13 +5,14 @@
 //  Created by Shunya Yamada on 2025/11/15.
 //
 
+import MapKit
 import SwiftUI
 
 public struct MovingMapView: View {
     @State private var viewModel: MovingMapViewModel
 
     public var body: some View {
-        Text("Hello, World!")
+        Map()
     }
 
     public init(viewModel: MovingMapViewModel) {
